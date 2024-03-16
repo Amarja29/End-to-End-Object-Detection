fbd88cd35ffb80f5e9ea2b4dd98a01dd6831f7f8
Amarja29 <amarjaindapwar.com>
2024-03-16 16:28:54 +0530

    new files added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'objectDetection',
+    version= '0.0.0',
+    author= 'Amarja Indapwar',
+    author_email= 'indapwaramarja@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
